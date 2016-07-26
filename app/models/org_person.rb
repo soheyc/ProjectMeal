ce714fe7d17eb3de7bac8445a9fe3f60956bd0e8
soheyc <sohey.creation@gmail.com>
2016-07-26 05:41:33 +0000

    Installed devise.

--- app/models/org_person.rb
+++ app/models/org_person.rb
@@ -1,2 +1,6 @@
 class OrgPerson < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
